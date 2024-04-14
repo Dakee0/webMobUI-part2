dbad698269b7bfd40c01caac2e1278abc6ef6989
Loïc Monnerat <loic.monnerat@heig-vd.ch>
2024-04-14 15:45:55 +0200

    Fin de la prise en main - Ajout du début du convertisseur BTC-CHF

--- prise-en-main/src/App.vue
+++ prise-en-main/src/App.vue
@@ -1,30 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, watch } from 'vue';
+
+const kelvin = ref(273.15);
+
+const kelvinComputed = computed({
+  get: () => kelvin.value.toFixed(2),
+  set: (value) => {
+    kelvin.value = parseFloat(value);
+  }
+});
+
+const celsius = computed({
+  get: () => (kelvin.value - 273.15).toFixed(2),
+  set: (newValue) => {
+    kelvin.value = parseFloat(newValue) + 273.15;
+  },
+});
+
+const farenheit = computed({
+  get: () => ((kelvin.value - 273.15) * 9 / 5 + 32).toFixed(2),
+  set: (newValue) => {
+    kelvin.value = (parseFloat(newValue) - 32) * 5 / 9 + 273.15;
+  },
+});
+
+
+watch(kelvin, (newValue) => {
+  console.log(`Kelvin: ${newValue}, Celsius: ${celsius.value}, Farenheit: ${farenheit.value}`);
+});
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>
+      Kelvin:
+      <input v-model.number="kelvinComputed" type="number" placeholder="Kelvin">
+    </p>
+    <p>
+      Celsius:
+      <input v-model.number="celsius" type="number" placeholder="Celsius">
+    </p>
+    <p>
+      Farenheit:
+      <input v-model.number="farenheit" type="number" placeholder="Farenheit">
+    </p>
+    <br>
+    <h2>Testings</h2>
+    <p>Celsius: {{ celsius }}</p>
+    <p>Farenheit: {{ farenheit }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
